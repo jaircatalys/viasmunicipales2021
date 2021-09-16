9c81724c5d0073424b4e3a109be2f9854a7b5fee
jaircatalys <jaircatalys@gmail.com>
2021-09-16 01:17:20 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
+
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TurnoController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\TipodevehiculoController;
+use App\Http\Controllers\NacionalidadController;
+use App\Http\Controllers\VehiculoController;
+use App\Http\Controllers\ConductorController;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\ReciboController;
+
+
+
+
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +30,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('turnos',[TurnoController::class,'index']);
+Route::get('turnos/create',[TurnoController::class,'create']);
+Route::get('turnos/{turno}',[TurnoController::class,'show']);
+
+Route::get('usuarios',[UsuarioController::class,'index']);
+Route::get('usuarios/create',[UsuarioController::class,'create']);
+Route::get('usuarios/{usuario}',[UsuarioController::class,'show']);
+
+Route::get('tiposdevehiculos',[TipodevehiculoController::class,'index']);
+Route::get('tiposdevehiculos/create',[TipodevehiculoController::class,'create']);
+Route::get('tiposdevehiculos/{tipodevehiculo}',[TipodevehiculoController::class,'show']);
+
+
+Route::get('Nacionalidad',[NacionalidadController::class,'index']);
+Route::get('Nacionalidad/create',[NacionalidadController::class,'create']);
+Route::get('Nacionalidad/{Nacionalidad}',[NacionalidadController::class,'show']);
+
+Route::get('Vehiculos',[VehiculoController::class,'index']);
+Route::get('Vehiculos/create',[VehiculoController::class,'create']);
+Route::get('Vehiculos/{Vehiculo}',[VehiculoController::class,'show']);
+
+
+Route::get('Conductores',[ConductorController::class,'index']);
+Route::get('Conductores/create',[ConductorController::class,'create']);
+Route::get('Conductores/{Conductor}',[ConductorController::class,'show']);
+
+
+Route::get('Empresas',[EmpresaController::class,'index']);
+Route::get('Empresas/empresa',[EmpresaController::class,'create']);
+Route::get('Empresas/{empresa}',[EmpresaController::class,'show']);
+
+Route::get('Recibos',[ReciboController::class,'index']);
+Route::get('Recibos/recibo',[ReciboController::class,'create']);
+Route::get('Recibos/recibo{}',[ReciboController::class,'show']);
